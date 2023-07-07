6a46fe1e5c8fd684a22cd5b6fe1650582b86a1a6
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-07-08 00:31:38 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFeedback = (event) => {
+    // console.dir(event);
+    console.dir(event.target.dataset.btn);
+    // console.dir(event.currentTarget.dataset);
+    this.setState(prevState => {
+   
+    
+
+      return { [event.target.dataset.btn]: prevState[event.target.dataset.btn] + 1 };
+    })
+      
+      
+      
+
+// this.setState({good: 1});
+
+
+   
+  }
+
+  render() {
+   
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <p>Please leave feedback</p>
+
+        <button type="button" data-btn="good" onClick={this.addFeedback}>
+          Good
+        </button>
+        <button type="button" data-btn="neutral" onClick={this.addFeedback}>
+          Neutral
+        </button>
+        <button type="button" data-btn="bad" onClick={this.addFeedback}>
+          Bad
+        </button>
+      </div>
+    );
+  }
 };
